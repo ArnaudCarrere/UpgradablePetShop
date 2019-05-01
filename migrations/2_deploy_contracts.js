5977d352647c352f4f9f838f320d5bdb2e950ad5
Arnaud <arnaud_carrere@hotmail.fr>
2019-04-30 19:09:45 -0700

    modifs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var Adoption = artifacts.require("Adoption");
-var UnstructuredProxy = artifacts.require("UnstructuredProxy");
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption);
-  deployer.deploy(UnstructuredProxy);
 };
